7c1e905557c53521dec94e1d512299233f014087
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-28 15:28:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".container");
+
+  // Create 800 boxes
+  for (let i = 0; i < 800; i++) {
+    const box = document.createElement("div");
+    box.classList.add("square");
+
+    // Add event listener for hover effect
+    box.addEventListener("mouseenter", function () {
+      // Change color on hover
+      box.style.backgroundColor = "rgb(29,29,29)";
+    });
+
+    // Add event listener for smooth transition after 1 second
+    box.addEventListener("mouseleave", function () {
+      setTimeout(() => {
+        box.style.backgroundColor = "#3498db";
+      }, 1000);
+    });
+
+    container.appendChild(box);
+  }
+});
