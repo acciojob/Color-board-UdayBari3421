5768c2804e5cd25eb71653b078771c7435800bf7
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-28 15:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector(".container");
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+// const colors = ['#ff7f50','#87cefa','#da70d6','#32cd32','#6495ed','#ff69b4','#ba55d3','#cd5c5c','#ffa500','#40e0d0']
 
-  // Create 800 boxes
-  for (let i = 0; i < 800; i++) {
-    const box = document.createElement("div");
-    box.classList.add("square");
 
-    // Add event listener for hover effect
-    box.addEventListener("mouseover", function () {
-      // Change color on hover
-      box.style.backgroundColor = "rgb(255,25,255)";
-    });
+const container  = document.getElementById("container")
 
-    // Add event listener for smooth transition after 1 second
-    box.addEventListener("mouseleave", function () {
-      setTimeout(() => {
-        box.style.backgroundColor = "#3498db";
-      }, 1000);
-    });
+const SQUARES = 800
 
-    container.appendChild(box);
-  }
-});
+for(let i=0;i<SQUARES;i++){
+    const square = document.createElement("div")
+    square.classList.add("square")
+
+    square.addEventListener("mouseover",()=> setColor(square))
+    square.addEventListener("mouseout",()=> removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element){
+    const color = randomColor()
+
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+}
+
+function removeColor(element){
+    element.style.background = "#1d1d1d"
+    element.style.boxShadow = "0 0 2px #000"
+}
+function randomColor(){
+    const index = Math.floor(Math.random()*colors.length)
+    return colors[index]
+}
